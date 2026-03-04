93d8481afe44e8e762dea563cc1f4281fd0683c8
rithmapehesara-lab <rithmapehesara0@gmail.com>
2026-03-05 03:11:40 +0530

    add PWA support

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,43 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'BusTrack SL',
+        short_name: 'BusTrack',
+        description: 'Sri Lanka Smart Bus Announcer',
+        theme_color: '#1d4ed8',
+        background_color: '#f0f4ff',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        icons: [
+          {
+            src: 'https://raw.githubusercontent.com/twitter/twemoji/master/assets/72x72/1f68c.png',
+            sizes: '72x72',
+            type: 'image/png'
+          },
+          {
+            src: 'https://raw.githubusercontent.com/twitter/twemoji/master/assets/72x72/1f68c.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'https://raw.githubusercontent.com/twitter/twemoji/master/assets/72x72/1f68c.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
+```
+
+**Ctrl+S → terminal:**
+```
